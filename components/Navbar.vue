9c73b07a110bdae07fbc673c41aec344361f14c5
Dote <10195318+valid22@users.noreply.github.com>
2024-10-06 11:03:15 +0530

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,8 +2,8 @@
     <nav class="bg-white border-b border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <div class="flex items-center">
-                <p class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white py-2">Riboswitch Sensor Tool</p>
-                <span class="bg-blue-100 text-blue-800 text-md font-medium ml-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">CompBio Lab</span>
+                <p class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white py-2">RiboSense: annotating genomic riboswitches</p>
+                <span class="bg-blue-100 text-blue-800 text-md font-medium ml-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Systems CompBio Lab</span>
                 <button data-tooltip-target="tooltip-default" id="theme-toggle" type="button" class="text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 md:hidden"
                     @click="$colorMode.preference = ($colorMode.value  == 'dark' ? 'light' : 'dark')">
                         <svg id="theme-toggle-dark-icon" class="dark:hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
